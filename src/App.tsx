8fc98c0d1e26e05391f0e9e1b59544b5cccd99bf
vip7009pro <hung1893@gmail.com>
2022-10-16 09:27:18 +0700

    them react spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css';
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import {useDispatch} from "react-redux"
 import {update} from "./redux/userSlice"
+import { animated, useSpring, useTransition } from '@react-spring/web'
 
 function App() {
   const user = useSelector((state:any)=>state.user);
   const dispatch = useDispatch(); 
+  const [show, setShow]= useState(true);
+  const springs = useSpring({
+    from: {x:0},
+    to: {x:100}
+  })
+
+
+  const transition = useTransition(show, {
+    from: {x: -100, y:300, opacity: 0},
+    enter: {x:0, y:0, opacity: 1},
+    leave: {x: -100, y:-300, opacity: 0},
+  })
 
   const handle_dispatchFunction=()=>{
     dispatch(update({
@@ -23,14 +36,18 @@ function App() {
   }, []);
   
   return (
-    <div className="App">
-      <header className="App-header">
-       Thong tin user: <br></br>
-       {user.name} <br></br>
-       {user.age} <br></br>
-       {user.about} <br></br>
-       {user.avatar} <br></br>
-      </header> 
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={()=>setShow(!show)}>Change show</button>
+        {
+          transition((style, item)=> item? <animated.div style={style} className='item'/>: '')
+        }
+        Thong tin user: <br></br>
+        {user.name} <br></br>
+        {user.age} <br></br>
+        {user.about} <br></br>
+        {user.avatar} <br></br>
+      </header>
     </div>
   );
 }
